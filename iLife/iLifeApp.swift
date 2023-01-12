adf1c9e629cc5a4b247bb3b2bc4f04c14723cf8c
Wiljo de Ruiter <wiljo.de.ruiter@ziggo.nl>
2023-01-12 19:01:46 +0100

    Initial Commit

---
+++ iLife/iLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iLifeApp.swift
+//  iLife
+//
+//  Created by Wiljo de Ruiter on 2023-01-12.
+//
+
+import SwiftUI
+
+@main
+struct iLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
